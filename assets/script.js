54bc748c43961252e80f62019cfd4849ccb8e856
Ieuan Sprigg-Wiggins <60429498+Ieuanspriggwiggins@users.noreply.github.com>
2023-07-03 14:10:22 +0100

    Header complete

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//Inputs for location entry
+const locationInput = document.getElementById('location-input');
+const locationInputSubmitBtn = document.getElementById('location-input-submit-btn');
+
+//Function to run when location is entered
+const locationSubmission = () => {
+
+}
+
+//Add event listener for when the submit button is pressed
+locationInputSubmitBtn.addEventListener('click' ,() => {
+
+});
