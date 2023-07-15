ba9767e4913c6b347cfcee46710866dbfe1cb983
Ieuan Sprigg-Wiggins <60429498+Ieuanspriggwiggins@users.noreply.github.com>
2023-07-15 12:05:45 +0100

    Add netlify function

---
+++ netlify/functions/weather.js
@@ -0,0 +1,8 @@
+exports.handler = async function (event, context) {
+    const value = process.env.WEATHER_API_KEY;
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: `Value of MY_IMPORTANT_VARIABLE is ${value}.` }),
+    };
+};
