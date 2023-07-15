c22abd36d38f23872ebc24da2b71896134059f04
Ieuan Sprigg-Wiggins <60429498+Ieuanspriggwiggins@users.noreply.github.com>
2023-07-15 12:23:28 +0100

    Testing

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,16 +1,13 @@
 exports.handler = async function (event, context) {
     const value = process.env.WEATHER_API_KEY;
 
-    //
-
     const location = event.queryStringParameters.location;
 
-
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(fetch(`https://api.weatherapi.com/v1/forecast.json?key=${value}&q=${location}&alerts=yes&days=5&aqi=yes`)
-            .then((response) => { return response.json() })
-            .then((response) => {return response})),
-    };
+    const request = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${value}&q=${location}&alerts=yes&days=5&aqi=yes`);
+    await request.json().then((response) => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response),
+        };
+    })
 };
