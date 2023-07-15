d38c971fcd0f4f5b3b8cd07ad8e7868cc34cbc28
Ieuan Sprigg-Wiggins <60429498+Ieuanspriggwiggins@users.noreply.github.com>
2023-07-15 12:17:00 +0100

    add weather

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,12 +1,16 @@
 exports.handler = async function (event, context) {
     const value = process.env.WEATHER_API_KEY;
 
-    //https://api.weatherapi.com/v1/forecast.json?key=${_weatherKey}&q=${locationName}&alerts=yes&days=5&aqi=yes
+    //
 
     const location = event.queryStringParameters.location;
 
+
+
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: `Value of MY_IMPORTANT_VARIABLE is ${location}.` }),
+        body: JSON.stringify(fetch(`https://api.weatherapi.com/v1/forecast.json?key=${value}&q=${locationName}&alerts=yes&days=5&aqi=yes`)
+            .then((response) => { return response.json() })
+            .then((response) => {return response})),
     };
 };
